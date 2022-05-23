7a2139ab6500a6cf4f41a068c1bb49d48666294d
AlamDK8 <101881110+AlamDK8@users.noreply.github.com>
2022-05-24 01:48:57 +0700

    Add files via upload

---
+++ Dosen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    public class Dosen
+    {
+
+        public int id;
+        public string nama;
+        public int Nik;
+        public string gender;
+        public string course;
+
+        public void dosen()
+        {
+            nama = "Kamarudin, M.Kom";
+            course = "Pemrograman";
+
+            
+        }
+
+        public void adddosen(string name, int nik)
+        {
+           Console.WriteLine("Nama : ");
+           name = Console.ReadLine();
+
+           Console.WriteLine("NIK : ");
+           nik = Convert.ToInt32(Console.ReadLine()); ;
+
+        }
+
+        public void addcourse (string ccourse)
+        {
+            Console.WriteLine("Course : ");
+            ccourse = Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace UTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen ();
+            Dosen dosen2 = new Dosen ();
+
+            dosen1.dosen();
+
+            Console.WriteLine("Nama   : {0}", dosen1.nama);
+            Console.WriteLine("Course : {0}", dosen1.course);
+
+            dosen2.adddosen(dosen2.nama, dosen2.Nik);
+
+            dosen2.addcourse(dosen2.course);
+
+            Thread.Sleep(2000);
+        }
+    }
+}
